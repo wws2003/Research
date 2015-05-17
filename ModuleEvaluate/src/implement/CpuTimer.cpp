1419a2325bf737a7a7c1e7e42b6c44b7107eecf9
pham <pham@apecsa-indonesia.com>
2015-05-17 20:23:05 +0900

    Fix the problem of wrong time measurement

--- ModuleEvaluate/src/implement/CpuTimer.cpp
+++ ModuleEvaluate/src/implement/CpuTimer.cpp
@@ -10,9 +10,9 @@
 
 double CpuTimer::getTimeInMs() {
 	struct timespec tm;
-	clock_gettime(CLOCK_REALTIME, &tm);
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tm);
 
-	return tm.tv_sec * 1000 + tm.tv_nsec / 1000;
+	return tm.tv_sec * 1000 + tm.tv_nsec / 1e6;
 }
 
 
