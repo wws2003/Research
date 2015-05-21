1cc62a845164bf085971d42db3fa819065ea3b8b
pham <pham@apecsa-indonesia.com>
2015-05-22 00:38:28 +0900

    Modify build with fPIC option (third attempt)

--- ModuleMathConcepts/Debug/src/implement/subdir.mk
+++ ModuleMathConcepts/Debug/src/implement/subdir.mk
@@ -23,7 +23,7 @@ CPP_DEPS += \
 src/implement/%.o: ../src/implement/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I../include/interface/abstract -I../include/interface/common -I../include/implement -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../include/interface/abstract -I../include/interface/common -I../include/implement -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -fPIC -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
