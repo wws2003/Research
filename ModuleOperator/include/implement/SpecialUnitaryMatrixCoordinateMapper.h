9acfc94fc91685b184ae98d049626023b40634bc
pham <pham@apecsa-indonesia.com>
2015-05-23 00:52:31 +0900

    Add special unitary matrix coordinate mapper prototype

---
+++ ModuleOperator/include/implement/SpecialUnitaryMatrixCoordinateMapper.h
@@ -0,0 +1,26 @@
+/*
+ * SpecialUnitaryMatrixCoordinateMapper.h
+ *
+ *  Created on: May 23, 2015
+ *      Author: pham
+ */
+
+#ifndef SPECIALUNITARYMATRIXCOORDINATEMAPPER_H_
+#define SPECIALUNITARYMATRIXCOORDINATEMAPPER_H_
+
+#include "IMatrixCoordinateMapper.h"
+#include "IMatrixOperator.h"
+
+class SpecialUnitaryMatrixCoordinateMapper : public IMatrixCoordinateMapper{
+public:
+	SpecialUnitaryMatrixCoordinateMapper(MatrixOperatorPtr pMatrixOperator);
+	virtual ~SpecialUnitaryMatrixCoordinateMapper(){};
+
+	virtual void mapMatrixToCoordinate(MatrixPtr pMatrix, MatrixCoordinatePtrRef prMatrixCoordinate) const;
+
+private:
+	MatrixOperatorPtr m_pMatrixOperator;
+};
+
+
+#endif /* SPECIALUNITARYMATRIXCOORDINATEMAPPER_H_ */
---
+++ ModuleOperator/src/implement/SpecialUnitaryMatrixCoordinateMapper.cpp
@@ -0,0 +1,19 @@
+/*
+ * SpecialUnitaryMatrixCoordinateMapper.cpp
+ *
+ *  Created on: May 23, 2015
+ *      Author: pham
+ */
+
+#include "SpecialUnitaryMatrixCoordinateMapper.h"
+
+SpecialUnitaryMatrixCoordinateMapper::SpecialUnitaryMatrixCoordinateMapper(MatrixOperatorPtr pMatrixOperator) {
+	m_pMatrixOperator = pMatrixOperator;
+}
+
+void SpecialUnitaryMatrixCoordinateMapper::mapMatrixToCoordinate(MatrixPtr pMatrix, MatrixCoordinatePtrRef prMatrixCoordinate) const {
+	//TODO Implement
+}
+
+
+
