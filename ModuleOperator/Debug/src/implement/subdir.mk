b78dd8e0156dbe5bb89a32bdfd1252d6ecd85ce2
pham <pham@apecsa-indonesia.com>
2015-12-12 20:24:43 +0900

    Remove optimization flag in compiler to debug

--- ModuleOperator/Debug/src/implement/subdir.mk
+++ ModuleOperator/Debug/src/implement/subdir.mk
@@ -50,7 +50,7 @@ CPP_DEPS += \
 src/implement/%.o: ../src/implement/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I../../ModuleMathConcepts/include/interface/abstract -I../../ModuleMathConcepts/include/interface/common -I../include/interface/abstract -I../include/interface/common -I../include/implement -I../../SharedThirdParties/gmp/include -I../../SharedThirdParties/mpfr/include -I../../SharedThirdParties/mpfrc++/include -I../third-parties -I../third-parties/eigen3 -O2 -g3 -Wall -c -fmessage-length=0 -std=c++11 -fPIC -ffloat-store -pg -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../ModuleMathConcepts/include/interface/abstract -I../../ModuleMathConcepts/include/interface/common -I../include/interface/abstract -I../include/interface/common -I../include/implement -I../../SharedThirdParties/gmp/include -I../../SharedThirdParties/mpfr/include -I../../SharedThirdParties/mpfrc++/include -I../third-parties -I../third-parties/eigen3 -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -fPIC -ffloat-store -pg -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
